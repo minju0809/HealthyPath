3ea7e21624ea58265c459e83accf15e0a0b470fc
minju0809 <loverminju88@naver.com>
2024-12-24 10:40:56 +0900

    fix: daily_meals 테이블에 컬럼 추가

--- src/main/java/com/springboot/healthypath/food/DailyMealVO.java
+++ src/main/java/com/springboot/healthypath/food/DailyMealVO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class DailyMealVO {
   private int meal_id;
   private Long user_id;
+  private int idx; 
   private String date;
   private String meal_time;
   private String food_name;
@@ -16,4 +17,9 @@ public class DailyMealVO {
   private float energy_kcal;
   private Date created_at;
   private Date updated_at;
+
+  private double protein_g; // 단백질 (g)
+  private double fat_g; // 지방 (g)
+  private double ash_g; // 회분 (g)
+  private double carbohydrate_g; // 탄수화물 (g)
 }
